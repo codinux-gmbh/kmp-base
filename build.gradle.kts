e26ca719c17aa7198aa3ae3b18f3d2cc687699a4
dankito <git@dankito.net>
2023-07-20 17:48:44 +0200

    Enabled Kotlin multiplatform target hierarchy

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,8 +9,13 @@ repositories {
     mavenCentral()
 }
 
+
 kotlin {
+    // Enable the default target hierarchy:
+    targetHierarchy.default()
+
     jvm {
+        jvmToolchain(8)
         compilations.all {
             kotlinOptions.jvmTarget = "1.8"
         }
@@ -19,25 +24,51 @@ kotlin {
             useJUnitPlatform()
         }
     }
-    js(BOTH) {
+
+    js(IR) {
+        moduleName = "kmp-base"
+        binaries.executable()
+
         browser {
             commonWebpackConfig {
                 cssSupport {
                     enabled.set(true)
                 }
             }
+            testTask {
+                useKarma {
+                    useChromeHeadless()
+                    useFirefoxHeadless()
+                }
+            }
+        }
+
+        nodejs {
+
         }
     }
-    val hostOs = System.getProperty("os.name")
-    val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
-        hostOs == "Mac OS X" -> macosX64("native")
-        hostOs == "Linux" -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-    }
 
-    
+
+    linuxX64()
+    mingwX64()
+
+
+    ios {
+        binaries {
+            framework {
+                baseName = "kmp-base"
+            }
+        }
+    }
+    iosSimulatorArm64()
+    macosX64()
+    macosArm64()
+    watchos()
+    watchosSimulatorArm64()
+    tvos()
+    tvosSimulatorArm64()
+
+
     sourceSets {
         val commonMain by getting
         val commonTest by getting {
@@ -45,11 +76,35 @@ kotlin {
                 implementation(kotlin("test"))
             }
         }
+
         val jvmMain by getting
         val jvmTest by getting
+
         val jsMain by getting
         val jsTest by getting
+
         val nativeMain by getting
         val nativeTest by getting
+    }
+}
+
+
+tasks.named<Test>("jvmTest") {
+    useJUnitPlatform()
+
+    maxParallelForks = 4
+//    maxHeapSize = "1G"
+
+    filter {
+        isFailOnNoMatchingTests = false
+    }
+    testLogging {
+        showExceptions = true
+        showStandardStreams = true
+        events = setOf(
+            org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED,
+            org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED
+        )
+        exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
     }
 }
