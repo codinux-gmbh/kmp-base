1240ad253fd3cc11134d06bd6e57269055caead8
dankito <git@dankito.net>
2025-01-04 23:20:31 +0100

    Moved PlatformType convenience properties to Platform

--- kmp-base/src/commonMain/kotlin/net/codinux/kotlin/Platform.kt
+++ kmp-base/src/commonMain/kotlin/net/codinux/kotlin/Platform.kt
@@ -1,5 +1,8 @@
 package net.codinux.kotlin
 
+import net.codinux.kotlin.Platform.type
+import net.codinux.kotlin.PlatformType.*
+
 expect object Platform {
 
     val type: PlatformType
@@ -17,3 +20,27 @@ expect object Platform {
     val cpuArchitecture: String?
 
 }
+
+
+private val DarwinPlatforms by lazy { setOf(macOS, iOS, tvOS, watchOS) }
+
+val Platform.isJvmOrAndroid
+    get() = type == JVM || type == Android
+
+val isJavaScript
+    get() = type == JavaScriptBrowser || type == JavaScriptNodeJS
+
+val isNative
+    get() = type == Linux || type == Windows || isDarwin
+
+val isLinuxOrMingw
+    get() = type == Linux || type == Windows
+
+val isDarwin
+    get() = DarwinPlatforms.contains(type)
+
+/**
+ * Alias for [isDarwin]
+ */
+val isAppleOS
+    get() = isDarwin
--- kmp-base/src/commonMain/kotlin/net/codinux/kotlin/PlatformType.kt
+++ kmp-base/src/commonMain/kotlin/net/codinux/kotlin/PlatformType.kt
@@ -13,24 +13,5 @@ enum class PlatformType {
     iOS,
     tvOS,
     watchOS
-    ;
-
-
-    private val DarwinPlatforms by lazy { setOf(macOS, iOS, tvOS, watchOS) }
-
-    val isJvmOrAndroid by lazy { this == JVM || this == Android }
-
-    val isJavaScript by lazy { this == JavaScriptBrowser || this == JavaScriptNodeJS }
-
-    val isNative by lazy { this == Linux || this == Windows || isDarwin }
-
-    val isLinuxOrMingw by lazy { this == Linux || this == Windows}
-
-    val isDarwin by lazy { DarwinPlatforms.contains(this) }
-
-    /**
-     * Alias for [isDarwin]
-     */
-    val isAppleOS by lazy { isDarwin }
 
 }
