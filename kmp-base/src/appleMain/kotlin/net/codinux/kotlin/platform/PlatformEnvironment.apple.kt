623385180eae5132115bcc9e7a6c504a88476330
dankito <git@dankito.net>
2025-01-05 19:34:48 +0100

    Fixed imports and mappping

--- kmp-base/src/appleMain/kotlin/net/codinux/kotlin/platform/PlatformEnvironment.apple.kt
+++ kmp-base/src/appleMain/kotlin/net/codinux/kotlin/platform/PlatformEnvironment.apple.kt
@@ -1,20 +1,18 @@
 package net.codinux.kotlin.platform
 
 import platform.Foundation.NSProcessInfo
-
-import net.codinux.kotlin.ApplePlatform.processInfo
-import platform.Foundation.NSProcessInfo
 import kotlin.experimental.ExperimentalNativeApi
 
 @OptIn(ExperimentalNativeApi::class)
 internal actual object PlatformEnvironment {
 
+    // TODO: does not work, XCTestConfigurationFilePath is never set when running tests with Gradle
     actual val isRunningTests = NSProcessInfo.processInfo.environment["XCTestConfigurationFilePath"] != null
 
     actual val isRunningInDebugMode = Platform.isDebugBinary
 
 
     actual fun getEnvironmentVariables(): Map<String, String> =
-        NSProcessInfo.processInfo.environment
+        NSProcessInfo.processInfo.environment.entries.associateBy({ it.key.toString() }, { it.value.toString() })
 
 }
