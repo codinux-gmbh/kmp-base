2bce5880004db00c80fa99b32f57737c38d769e6
dankito <git@dankito.net>
2025-01-05 19:35:52 +0100

    Fixed Android.isRunningInDebugMode so that it also runs in Android unit tests where applicationContext is not available

--- kmp-base/src/androidMain/kotlin/net/codinux/kotlin/android/AndroidContext.kt
+++ kmp-base/src/androidMain/kotlin/net/codinux/kotlin/android/AndroidContext.kt
@@ -4,4 +4,12 @@ import android.content.Context
 
 object AndroidContext {
     lateinit var applicationContext: Context
+
+    fun getApplicationContextIfInitialized(): Context? =
+        if (AndroidContext::applicationContext.isInitialized) {
+            applicationContext
+        } else {
+            null
+        }
+
 }
--- kmp-base/src/androidMain/kotlin/net/codinux/kotlin/platform/JavaPlatformExpects.android.kt
+++ kmp-base/src/androidMain/kotlin/net/codinux/kotlin/platform/JavaPlatformExpects.android.kt
@@ -1,13 +1,15 @@
 package net.codinux.kotlin.platform
 
+import android.content.pm.ApplicationInfo
 import net.codinux.kotlin.android.AndroidContext
 
 internal actual object JavaPlatformExpects {
 
-    actual val isRunningInDebugMode: Boolean
-        // BuildConfig.DEBUG will always be false as for a compiled library its set to false at compile time
-        AndroidContext.applicationContext?.applicationContext?.let {
-            it.applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE != 0
+    actual val isRunningInDebugMode: Boolean =
+        AndroidContext.getApplicationContextIfInitialized()?.let { applicationContext ->
+            // BuildConfig.DEBUG will always be false as for a compiled library its set to false at compile time
+            val applicationInfo = applicationContext.applicationContext.applicationInfo
+            applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE != 0
         } ?: false
 
 }
