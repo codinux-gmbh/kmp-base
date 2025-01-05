44a38849a4f450f8fc610822f326d005e818041c
dankito <git@dankito.net>
2025-01-05 19:43:39 +0100

    Bumped version to 1.1.1-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ buildscript {
 
 allprojects {
     group = "net.codinux.kotlin"
-    version = "1.1.0"
+    version = "1.1.1-SNAPSHOT"
 
     ext["sourceCodeRepositoryBaseUrl"] = "github.com/codinux-gmbh/kmp-base"
 
